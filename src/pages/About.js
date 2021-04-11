5197d22a2d41d331a2339f7f4dcb9cdcbe133b92
s0rta <jacob.t.wright@ucdenver.edu>
2021-04-11 17:31:18 -0600

    credit & about page, broked images fixed

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,8 +8,8 @@ class About extends React.Component {
   {
       return (
     <div>
-        <h1>Welcome to The Salt Marsh Game!</h1>
-        <Menu/>
+        <h1>About</h1>
+        <p>Welcome to the salt marsh game, this is a small tool to help build understanding about food webs and their relation to environmental services. In this game, a disturbance will be enacted upon a salt marsh and it will be up to you to intervene, saving species in an attempt to mitigate damage to the web and services humanity relys on. This game is in very early alpha so if you have any feedback, please direct it to jacob.t.wright(at)ucdenver.edu with the subject 'salt marsh game'.</p>
     </div>
         )
         }
--- src/pages/Credits.js
+++ src/pages/Credits.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Menu from '../components/Menu/Menu';
 
-import "./MainMenu.css"
+import "./credits.css"
+import { imgCredit } from '../data/lists'
 
 class Credits extends React.Component {
   render()
   {
       return (
     <div>
-        <h1>Welcome to The Salt Marsh Game!</h1>
-        <Menu/>
+        <h1>Credits</h1>
+        <p>This game was thought up and created by Aislyn Keyes, with programming help from JT Wright.</p>
+        <h2>Image Credits</h2>
+        <ul>
+      {imgCredit.map((value, index) => {
+        return <li key={index}><img src={`/img/Images/${value.imgFile}`} alt={value.imgCaption}/> <p>{value.imgSource} {value.imgLiscence}</p></li>
+      })}
+    </ul>
     </div>
         )
         }
