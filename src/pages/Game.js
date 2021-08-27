93239978514a40087d199c870ba1ac52003d9e45
JT <jacob.t.wright@protonmail.com>
2021-08-27 14:41:28 -0700

    data sets corrected

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,172 +1,192 @@
-import React from 'react';
-import SideBar from '../components/SideBar.js'
-import MainGraph from '../components/MainGraph.js'
-import SubGraphs from '../components/SubGraphs.js'
-import Modal from 'react-modal'
-import * as lists from "../data/lists"
+import React from "react";
+import SideBar from "../components/SideBar.js";
+import MainGraph from "../components/MainGraph.js";
+import SubGraphs from "../components/SubGraphs.js";
+import Modal from "react-modal";
+import * as lists from "../data/lists";
 
-import "./Game.css"
-import {Link} from 'react-router-dom';
+import "./Game.css";
+import { Link } from "react-router-dom";
 
 class Game extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        let [levelNodes,
-            levelEdges, levelData] = this.getLevelData();
+    let [levelNodes, levelEdges, levelData] = this.getLevelData();
 
-        this.state = {
-            level: this.props.match.params.level,
-            levelNodes: levelNodes,
-            levelEdges: levelEdges,
-            levelData: levelData,
-            speciesRemaining: -1,
-            subGraphNodes: [],
-            subGraphEdges: [],
-            subSeed: 0,
-            esBiomass: [-1, -1, -1, -1, -1, -1, -1, -1],
-            step: 0,
-            isModalOpen: true,
-            levelOver: false,
-            levelWon: false,
-        };
+    this.state = {
+      level: this.props.match.params.level,
+      levelNodes: levelNodes,
+      levelEdges: levelEdges,
+      levelData: levelData,
+      speciesRemaining: -1,
+      subGraphNodes: [],
+      subGraphEdges: [],
+      subSeed: 0,
+      esBiomass: [-1, -1, -1, -1, -1, -1, -1, -1],
+      step: 0,
+      isModalOpen: true,
+      levelOver: false,
+      levelWon: false,
+    };
+  }
+
+  getLevelData = () => {
+    let levelNodes;
+    let levelEdges;
+    let levelData;
+
+    switch (this.props.match.params.level) {
+      case "1":
+        levelNodes = lists.nodeList1;
+        levelEdges = lists.edgeList1;
+        levelData = lists.levels[0];
+        break;
+      case "2":
+        levelNodes = lists.nodeList2;
+        levelEdges = lists.edgeList2;
+        levelData = lists.levels[1];
+        break;
+      case "3":
+        levelNodes = lists.nodeList3;
+        levelEdges = lists.edgeList3;
+        levelData = lists.levels[2];
+        break;
+      case "4":
+        levelNodes = lists.nodeList4;
+        levelEdges = lists.edgeList4;
+        // levelData = lists.levels[3][Math.floor(Math.random() * 4)];
+        levelData = lists.levels[3][0];
+        break;
+      case "5":
+        levelNodes = lists.nodeList5;
+        levelEdges = lists.edgeList5;
+        // levelData = lists.levels[4][Math.floor(Math.random() * 4)];
+        levelData = lists.levels[4][1];
+        break;
+      case "6":
+        levelNodes = lists.nodeList6;
+        levelEdges = lists.edgeList6;
+        // levelData = lists.levels[5][Math.floor(Math.random() * 4)];
+        levelData = lists.levels[5][2];
+        break;
+      case "7":
+        levelNodes = lists.nodeList7;
+        levelEdges = lists.edgeList7;
+        // levelData = lists.levels[6][Math.floor(Math.random() * 4)];
+        levelData = lists.levels[6][3];
+        break;
+      default:
+        break;
     }
+    console.log(levelEdges);
+    return [levelNodes, levelEdges, levelData];
+  };
 
-    getLevelData = () => {
-        let levelNodes;
-        let levelEdges;
-        let levelData;
+  componentDidMount() {}
 
-        switch (this.props.match.params.level) {
-            case "1":
-                levelNodes = lists.nodeList1;
-                levelEdges = lists.edgeList1;
-                levelData = lists.levels[0];
-                break;
-            case "2":
-                levelNodes = lists.nodeList2;
-                levelEdges = lists.edgeList2;
-                levelData = lists.levels[1];
-                break;
-            case "3":
-                levelNodes = lists.nodeList3;
-                levelEdges = lists.edgeList3;
-                levelData = lists.levels[2];
-                break;
-            case "4":
-                levelNodes = lists.nodeList4;
-                levelEdges = lists.edgeList4;
-                // levelData = lists.levels[3][Math.floor(Math.random() * 4)];
-                levelData = lists.levels[3][0];
-                break;
-            case "5":
-                levelNodes = lists.nodeList5;
-                levelEdges = lists.edgeList5;
-                // levelData = lists.levels[4][Math.floor(Math.random() * 4)];
-                levelData = lists.levels[4][0];
-                break;
-            case "6":
-                levelNodes = lists.nodeList6;
-                levelEdges = lists.edgeList6;
-                // levelData = lists.levels[5][Math.floor(Math.random() * 4)];
-                levelData = lists.levels[5][0];
-                break;
-            case "7":
-                levelNodes = lists.nodeList7;
-                levelEdges = lists.edgeList7;
-                // levelData = lists.levels[6][Math.floor(Math.random() * 4)];
-                levelData = lists.levels[6][0];
-                break;
-            default:
-                break;
-        }
-        console.log(levelEdges)
-        return [levelNodes, levelEdges, levelData]
+  handleLevelEnd = (d) => {
+    this.setState({ levelWon: d, levelOver: true });
+  };
+
+  handleNodeHover = (d) => {
+    this.setState((state, props) => {
+      return { hoveredNode: d };
+    });
+  };
+
+  handleRightClick = (d, seed) => {
+    this.setState((state, props) => {
+      return { subGraphEpi: d, subSeed: seed };
+    });
+  };
+
+  handleESBiomass = (bioArr, step) => {
+    this.setState({ esBiomass: bioArr, step: step });
+  };
+
+  handleSpeciesRemaining = (count) => {
+    this.setState({ speciesRemaining: count - 8 });
+  };
+
+  swapModal = () => {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  };
+
+  closeEndModal = () => {
+    this.setState({ levelOver: false });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.match.params.level !== prevProps.match.params.level) {
+      window.location.reload();
     }
+  }
 
-    componentDidMount() {}
+  render() {
+    const winTarget = {
+      pathname: `/game/${parseFloat(this.props.match.params.level) + 1}`,
+    };
 
-    handleLevelEnd = (d) => {
-        this.setState({levelWon: d, levelOver: true})
-    }
-
-    handleNodeHover = (d) => {
-        this.setState((state, props) => {
-            return {hoveredNode: d}
-        })
-    }
-
-    handleRightClick = (d, seed) => {
-        this.setState((state, props) => {
-            return {subGraphEpi: d, subSeed: seed}
-        })
-    }
-
-    handleESBiomass = (bioArr, step) => {
-        this.setState({esBiomass: bioArr, step: step})
-    }
-
-    handleSpeciesRemaining = (count) => {
-        this.setState({speciesRemaining: count - 8})
-    }
-
-    swapModal = () => {
-        this.setState({isModalOpen: !this.state.isModalOpen})
-    }
-
-    closeEndModal = () => {
-        this.setState({levelOver: false})
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if(this.props.match.params.level !== prevProps.match.params.level) {
-            window.location.reload()
-        }
-    }
-
-    render()
-    {
-
-        const winTarget = {
-            pathname: `/game/${parseFloat(this.props.match.params.level) + 1}`,
-        };
-
-        return (
-            <div className="game-wrap">
-                <Modal isOpen={this.state.isModalOpen} className="levelModal">
-                    <h1>Level {this.state.levelData.level} Intro</h1>
-                    <p>{this.state.levelData.intro}</p>
-                    <h1>Objective</h1>
-                    <p>{this.state.levelData.objective}</p>
-                    <button onClick={() => this.swapModal()}>Sounds Good</button>
-                </Modal>
-                <Modal isOpen={this.state.levelOver} className="levelModal">
-                    {this.state.levelWon ? this.state.levelData.win : this.state.levelData.lose}<br/> 
-                    {this.state.levelWon && <Link to={winTarget}>
-                        <button>Next Level</button>
-                    </Link>}
-                    <button onClick={() => window.location.reload()}>Restart Level</button>
-                    <button onClick={() => this.closeEndModal()}>Explore</button>
-                </Modal>
-                <SideBar onToggleModal={this.swapModal} level={this.state.level} data={this.state.hoveredNode}/>
-                <MainGraph
-                    levelData={this.state.levelData}
-                    levelOver={this.state.levelOver}
-                    winTarget={winTarget}
-                    won={this.state.levelWon}
-                    colors={lists.colors}
-                    nodes={[...this.state.levelNodes]}
-                    edges={[...this.state.levelEdges]}
-                    onNodeHover={this.handleNodeHover}
-                    onRightClick={this.handleRightClick}
-                    onUpdateESBiomass={this.handleESBiomass}
-                    onUpdateSpeciesRemaining={this.handleSpeciesRemaining}
-                    onLevelEnd={this.handleLevelEnd}/>
-                <SubGraphs onLevelEnd={this.handleLevelEnd} levelData={this.state.levelData} esBiomass={this.state.esBiomass} step={this.state.step} onNodeHover={this.handleNodeHover} epiNode={this.state.subGraphEpi} seed={this.state.subSeed} colors={lists.colors} nodes={[...this.state.levelNodes]}
-                    edges={[...this.state.levelEdges]} speciesRemaining={this.state.speciesRemaining}/>
-            </div>
-        )
-    }
+    return (
+      <div className="game-wrap">
+        <Modal isOpen={this.state.isModalOpen} className="levelModal">
+          <h1>Level {this.state.levelData.level} Intro</h1>
+          <p>{this.state.levelData.intro}</p>
+          <h1>Objective</h1>
+          <p>{this.state.levelData.objective}</p>
+          <button onClick={() => this.swapModal()}>Sounds Good</button>
+        </Modal>
+        <Modal isOpen={this.state.levelOver} className="levelModal">
+          {this.state.levelWon
+            ? this.state.levelData.win
+            : this.state.levelData.lose}
+          <br />
+          {this.state.levelWon && (
+            <Link to={winTarget}>
+              <button>Next Level</button>
+            </Link>
+          )}
+          <button onClick={() => window.location.reload()}>
+            Restart Level
+          </button>
+          <button onClick={() => this.closeEndModal()}>Explore</button>
+        </Modal>
+        <SideBar
+          onToggleModal={this.swapModal}
+          level={this.state.level}
+          data={this.state.hoveredNode}
+        />
+        <MainGraph
+          levelData={this.state.levelData}
+          levelOver={this.state.levelOver}
+          winTarget={winTarget}
+          won={this.state.levelWon}
+          colors={lists.colors}
+          nodes={[...this.state.levelNodes]}
+          edges={[...this.state.levelEdges]}
+          onNodeHover={this.handleNodeHover}
+          onRightClick={this.handleRightClick}
+          onUpdateESBiomass={this.handleESBiomass}
+          onUpdateSpeciesRemaining={this.handleSpeciesRemaining}
+          onLevelEnd={this.handleLevelEnd}
+        />
+        <SubGraphs
+          onLevelEnd={this.handleLevelEnd}
+          levelData={this.state.levelData}
+          esBiomass={this.state.esBiomass}
+          step={this.state.step}
+          onNodeHover={this.handleNodeHover}
+          epiNode={this.state.subGraphEpi}
+          seed={this.state.subSeed}
+          colors={lists.colors}
+          nodes={[...this.state.levelNodes]}
+          edges={[...this.state.levelEdges]}
+          speciesRemaining={this.state.speciesRemaining}
+        />
+      </div>
+    );
+  }
 }
 
 export default Game;
--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Menu from '../components/Menu/Menu';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import "./MainMenu.css"
+import "./MainMenu.css";
 
 class Intro extends React.Component {
-  render()
-  {
-      return (
-    <div className="introWrap">
+  render() {
+    return (
+      <div className="introWrap">
         <div>
-        <h1>Welcome to The Salt Marsh Game</h1>
-        <p>Here is a quick introduction to the concepts being covered in this game.</p>
-        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRrzy4H462RfSOb5YjfLu78PvfrfG8oyT12dWqXB7-OFiSQWE_QaowPGxDIDglG1g/embed?start=true&loop=false&delayms=600000" frameborder="0" width="1186" height="691" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>        <br/>
-        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTjnHXWNPf4VVlDN5pzfd6dUdSzjb4CDzbXN7VBDRRK-j3J_O0xW0-6nzktj0I2AA/embed?start=true&loop=false&delayms=600000" frameborder="0" width="1186" height="691" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>        <br/>
-        <Link to="/game/1">
+          <h1>Welcome to The Salt Marsh Game</h1>
+          <p>
+            Here is a quick introduction to the concepts being covered in this
+            game.
+          </p>
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vQVj-gvBCddOlU0exsiBJM_PCxX7wsGbitrdN9AxKfCR0J4gAJNKSyJzUEMtBci7JAC1Y3--NyGRXtA/embed?start=false&loop=false&delayms=600000"
+            frameborder="0"
+            width="1186"
+            height="691"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>{" "}
+          <br />
+          <iframe
+            src="https://docs.google.com/presentation/d/e/2PACX-1vTjnHXWNPf4VVlDN5pzfd6dUdSzjb4CDzbXN7VBDRRK-j3J_O0xW0-6nzktj0I2AA/embed?start=true&loop=false&delayms=600000"
+            frameborder="0"
+            width="1186"
+            height="691"
+            allowfullscreen="true"
+            mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+          ></iframe>{" "}
+          <br />
+          <Link to="/game/1">
             <button>Start Game</button>
-        </Link>
+          </Link>
         </div>
-    </div>
-        )
-        }
-    }
+      </div>
+    );
+  }
+}
 
 export default Intro;
+
