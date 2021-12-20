12930174269250d1553adfcd593ef07581394226
yeT <jacob.t.wright@protonmail.com>
2021-12-19 18:40:04 -0600

    tutorial text added

--- src/pages/Tutorial.js
+++ src/pages/Tutorial.js
@@ -10,8 +10,10 @@ class Tutorial extends React.Component {
         <div>
           <h1>Tutorial</h1>
           <br />
-<iframe width="560" height="315" src="https://www.youtube.com/embed/AQiwBLUMrf0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-{" "}
+          <p>In this game, you will use information about species and their interactions to decide which species to protect to maintain ecosystem services. Each level will get progressively more challenging and will focus on a different ecosystem service. Watch this short video to learn how to play.</p>
+          <br />
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/AQiwBLUMrf0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          {" "}
           <br />
           <Link to="/game/1">
             <button class="btn--primary">Start Game</button>
